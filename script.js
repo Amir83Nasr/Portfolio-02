0d8324a0b84467c52243f164f2d8848d55757658
Amir Hossein Nasrollahi <ah12.nasrollahi@gmail.com>
2023-12-02 00:53:10 +0330

    Dark Mode

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ function openMenu() {
     
     nav.classList.toggle("open")
 }
+
+let icon = document.getElementById("icon")
+
+icon.onclick = function() {
+    document.body.classList.toggle("dark-theme")
+
+    if (document.body.classList.contains("dark-theme")) icon.className = "bx bxs-moon" 
+    else icon.className = "bx bxs-sun" 
+}
